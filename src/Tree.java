2be5269b16f70e45911327b88c3bbdba2b1090ed
Juuso Hyyryläinen <juusohyyrylainen@hotmail.com>
2023-11-11 22:40:12 +0200

    tree nodes added

---
+++ src/Tree.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+
+
+public class Tree{
+    // Hands out operations to tree
+    private Node head;
+
+    public Tree(){
+        this.head = new Node(null);
+    }
+    
+    public boolean searchForWords(String word){
+        return True;
+    }
+
+
+}
